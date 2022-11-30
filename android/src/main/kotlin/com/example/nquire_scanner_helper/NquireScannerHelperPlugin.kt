0cd5ffbe795efe37a0249827c4a3b891e96c7f58
Emanuele Vinci <emanuele.vinci.dev@gmail.com>
2022-11-30 11:39:55 +0100

    fix

--- android/src/main/kotlin/com/example/nquire_scanner_helper/NquireScannerHelperPlugin.kt
+++ android/src/main/kotlin/com/example/nquire_scanner_helper/NquireScannerHelperPlugin.kt
@@ -1,40 +1,41 @@
 package com.example.nquire_scanner_helper
 
+import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** NquireScannerHelperPlugin */
-class NquireScannerHelperPlugin : FlutterPlugin, MethodCallHandler {
+class NquireScannerHelperPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     private lateinit var channel: MethodChannel
+    private lateinit var context: Context
+    private lateinit var activity: Activity
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "nquire_scanner_helper")
         channel.setMethodCallHandler(this)
+        context = flutterPluginBinding.applicationContext
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-
-
         val scannerReceiverWrapper: BroadcastReceiverWrapper = BroadcastReceiverWrapper()
 
-        channel.setMethodCallHandler { call, result ->
-            when {
-                call.method.equals("activateScan") -> {
-                    activateScan(result, scannerReceiverWrapper)
-                }
+        when {
+            call.method.equals("activateScan") -> {
+                activateScan(result, scannerReceiverWrapper)
+            }
 
-                call.method.equals("stopScan") -> {
-                    stopScan(scannerReceiverWrapper.receiver)
-                }
+            call.method.equals("stopScan") -> {
+                stopScan(scannerReceiverWrapper.receiver)
             }
         }
     }
@@ -46,8 +47,7 @@ class NquireScannerHelperPlugin : FlutterPlugin, MethodCallHandler {
     ) {
         val intent = Intent("nlscan.action.SCANNER_TRIG")
         intent.putExtra("SCAN_TIMEOUT", 9) // SCAN_TIMEOUT value: int, 1-9; unit: second.
-        sendBroadcast(intent)
-        println("Scan started")
+        context.sendBroadcast(intent)
 
         try {
             scannerReceiverWrapper.receiver = object : BroadcastReceiver() {
@@ -58,7 +58,7 @@ class NquireScannerHelperPlugin : FlutterPlugin, MethodCallHandler {
             }
 
             val mFilter = IntentFilter("nlscan.action.SCANNER_RESULT")
-            registerReceiver(scannerReceiverWrapper.receiver, mFilter)
+            context.registerReceiver(scannerReceiverWrapper.receiver, mFilter)
             println("Listener started, waiting for scan...")
         } catch (e: Exception) {
             println("LISTENER ERROR:")
@@ -68,8 +68,8 @@ class NquireScannerHelperPlugin : FlutterPlugin, MethodCallHandler {
 
     private fun stopScan(scannerReceiver: BroadcastReceiver) {
         val stopIntent = Intent("nlscan.action.STOP_SCAN")
-        sendBroadcast(stopIntent)
-        unregisterReceiver(scannerReceiver)
+        context.sendBroadcast(stopIntent)
+        context.unregisterReceiver(scannerReceiver)
 
         println("Stop Scanning")
     }
@@ -77,6 +77,19 @@ class NquireScannerHelperPlugin : FlutterPlugin, MethodCallHandler {
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity;
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    }
+
+    override fun onDetachedFromActivity() {
     }
 }
 
